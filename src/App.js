953f629b23893302046b2836b5b5ab5578e8710c
Zulaika <zulaika.ali@outlook.com>
2022-10-10 14:38:13 +0100

    add form design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { default as Socket } from './Socket-io'
+import { Settings } from './pages'
 
+import './App.css'
 function App() {
-
+ 
 
   return (
     <div>
+      <Settings />
       <Socket />
     </div>
   );
