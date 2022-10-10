4918923a538977db7e28e5598350630584226532
Littlerou <izzymelvin4@gmail.com>
2022-10-10 14:52:39 +0100

    change port to 3000

---
+++ quzzical/src/App.js
@@ -0,0 +1,17 @@
+import { default as Socket } from './Socket-io'
+import { Settings } from './pages'
+
+import './App.css'
+function App() {
+ 
+
+  return (
+    <div>
+      <Settings />
+      <Socket />
+    </div>
+  );
+
+}
+
+export default App;
