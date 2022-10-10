df00ea7bcf2f675fd56e8e56f58e79119fd59a13
Zulaika <zulaika.ali@outlook.com>
2022-10-10 11:39:11 +0100

    add socket.io in its own file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { default as Socket } from './Socket-io'
 
 function App() {
-  return ( 
-    <>
-    <Socket />
-    </>
+
+
+  return (
+    <div>
+      <Socket />
+    </div>
   );
+
 }
 
 export default App;
