e4c9f202ce97a2fbcfd491647e7c70628821abdd
Littlerou <izzymelvin4@gmail.com>
2022-10-11 14:04:44 +0100

    add room functionality

--- quzzical/src/Socket-io.js
+++ quzzical/src/Socket-io.js
@@ -6,6 +6,9 @@ const socket = io('http://localhost:5001');
 const Socket = () => {
     const [isConnected, setIsConnected] = useState(socket.connected);
     const [lastPong, setLastPong] = useState(null);
+    const [roomName, setRoomName] = useState(null);
+    const [numPlayers, setNumPlayers] = useState(0)
+    
 
     useEffect(() => {
         socket.on('connect', () => {
@@ -13,14 +16,23 @@ const Socket = () => {
         });
 
         socket.on('admin-message', msg => console.log(msg))
+        socket.on('create error', msg => console.log(msg))
+        socket.on('join error', msg => console.log(msg))
+        socket.on('ready message', msg => console.log(msg))
+
+        socket.on('room size', data => {
+            setNumPlayers(data)
+        } )
 
         socket.on('disconnect', () => {
             setIsConnected(false);
         });
 
         socket.on('pong', () => {
-            setLastPong(new Date().toISOString());
+            setLastPong('hello there');
         });
+
+       
 
         return () => {
             socket.off('connect');
@@ -29,15 +41,43 @@ const Socket = () => {
         };
     }, []);
 
+    const handleChange = (e) => {
+        setRoomName(e.target.value)
+        
+    }
+
     const sendPing = () => {
-        socket.emit('ping');
+        socket.emit('ping', 'heyyyy');
+    }
+
+    const joinRoom = () => {
+        socket.emit('join room', {room:roomName})
+    }
+    const createRoom = () => {
+        
+        socket.emit('create room', {room:roomName})
+        
+        
+    }
+
+    
+    const BeginGame = () => {
+        socket.emit('player ready', {room: roomName, player:'player 1'})
     }
 
     return (
         <div>
             <p>Connected: {'' + isConnected}</p>
             <p>Last pong: {lastPong || '-'}</p>
+            <p>Number of players: {numPlayers}</p>
             <button onClick={sendPing}>Send ping</button>
+            <button onClick={joinRoom}>Join Room</button>
+            <button onClick={createRoom}>Create Room</button>
+            <input type="text" onChange={handleChange} style={{backgroundColor:'white', color:'black'}}></input>
+            <button onClick={BeginGame}>Begin game</button>
+            
+            
+            
         </div>
     )
 };
