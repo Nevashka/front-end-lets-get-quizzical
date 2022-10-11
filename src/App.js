4d138896b239daad2dc3157b5197b384a013aec1
Zulaika <zulaika.ali@outlook.com>
2022-10-11 11:01:37 +0100

    add settings form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { default as Socket } from './Socket-io'
+// import { default as Socket } from './Socket-io'
 import { Settings } from './pages'
+import { Routes, Route } from 'react-router-dom'
 
 import './App.css'
+
 function App() {
- 
 
   return (
-    <div>
-      <Settings />
-      <Socket />
-    </div>
+    <>
+      <Routes>
+        <Route path="settings" element={<Settings />} />
+      </Routes>
+    </>
   );
 
 }
